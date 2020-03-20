9990c40317bd24a7abd748892c5c5c3fe60f6794
kamirob <camilobastidas1996@gmail.com>
2020-03-20 07:35:24 -0500

    Add Proyect

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      
+    </>
+  );
+};
+
+export default App;
